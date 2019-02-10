c5f94aca612f9358b9f3689dd6025ce87ce54529
ironpup <christoph.ding@gmail.com>
2019-02-10 15:17:54 -0500

    Add a devstart using nodemon.  Add some comments to app.js to better understand each part.

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -1,24 +1,31 @@
+// some useful node libraries
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//  modules from our routes directory. These modules/files contain code for handling particular sets of related "routes" (URL paths)
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// create the app object using our imported express module
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// add middleware libraries into the request handling chain
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// serve all the static files in the /public directory in the project root
 app.use(express.static(path.join(__dirname, 'public')));
 
+// add our (previously imported) route-handling code to the request handling chain
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -38,4 +45,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Express application object (app) is now fully configured
+// this is what allows it to be imported by /bin/www
 module.exports = app;
