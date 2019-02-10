6520995c9289d83f176316a0714ea9bb0d538a06
ironpup <christoph.ding@gmail.com>
2019-02-10 15:54:42 -0500

    Add some comments to understand the router functions.

--- express-locallibrary-tutorial/routes/index.js
+++ express-locallibrary-tutorial/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+   // that route renders a response using the template "index" passing the template variable "title".
   res.render('index', { title: 'Express' });
 });
 
--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// a callback invoked whenever an HTTP GET request with the correct pattern is detected
+
 router.get('/', function(req, res, next) {
+  // the callback function has the third argument 'next', and is hence a middleware function rather than a simple route callback
   res.send('respond with a resource');
 });
 
+// this is what allows the file to be imported into app.js
 module.exports = router;
