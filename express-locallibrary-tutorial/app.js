881b5c87a4bf3e4ff54153725b6f02e5ead198f2
ironpup <christoph.ding@gmail.com>
2019-02-11 15:31:01 -0500

    Add mongodb connection and mongoose.

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -12,6 +12,14 @@ var usersRouter = require('./routes/users');
 // create the app object using our imported express module
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://test_user:testone@locallibrary-7zkfe.mongodb.net/test?retryWrites=true';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
